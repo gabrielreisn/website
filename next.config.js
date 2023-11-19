9f134b581f795601efb84b5145a7adc124c8138b
gabriel reis <gabrielreisnf@gmail.com>
2023-11-19 16:42:06 -0300

    chore: push portfolio redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: [
+    {
+      source: '/portfolio',
+      destination: 'https://gabrielreisnf.notion.site/Gabriel-Portfolio-5d94072f658e4e9e95c8b88e6d103fa4',
+      permanent: true,
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
